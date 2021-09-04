f53af967639a0258689dd35d97b1408a0bc949a5
Victor Soeiro <victor.soeiro.araujo@gmail.com>
2021-09-03 23:30:24 -0300

    Remove sess argument

--- cornershop_scraper/core/objects/countries.py
+++ cornershop_scraper/core/objects/countries.py
@@ -7,18 +7,18 @@ includint its initials and language.
 """
 
 ACCEPTED_COUNTRIES = [
-    {'country': 'AR', 'language': '', 'name': 'Argentina'},
+    {'country': 'AR', 'language': 'es-ar', 'name': 'Argentina'},
     {'country': 'BR', 'language': 'pt-br', 'name': 'Brasil'},
     {'country': 'CA', 'language': 'en-ca', 'name': 'Canada'},
-    {'country': 'CL', 'language': '', 'name': 'Chile'},
-    {'country': 'CO', 'language': '', 'name': 'Colombia'},
-    {'country': 'CR', 'language': '', 'name': 'Costa Rica'},
-    {'country': 'EC', 'language': '', 'name': 'Ecuador'},
-    {'country': 'MX', 'language': '', 'name': 'México'},
-    {'country': 'PA', 'language': '', 'name': 'Panamá'},
-    {'country': 'PY', 'language': '', 'name': 'Paraguay'},
-    {'country': 'PE', 'language': '', 'name': 'Perú'},
+    {'country': 'CL', 'language': 'es-cl', 'name': 'Chile'},
+    {'country': 'CO', 'language': 'es-co', 'name': 'Colombia'},
+    {'country': 'CR', 'language': 'es-cr', 'name': 'Costa Rica'},
+    {'country': 'EC', 'language': 'es-ec', 'name': 'Ecuador'},
+    {'country': 'MX', 'language': 'ex-mx', 'name': 'México'},
+    {'country': 'PA', 'language': 'es-pa', 'name': 'Panamá'},
+    {'country': 'PY', 'language': 'es-py', 'name': 'Paraguay'},
+    {'country': 'PE', 'language': 'es-pe', 'name': 'Perú'},
     {'country': 'US', 'language': 'en-us', 'name': 'United States'},
-    {'country': 'UY', 'language': '', 'name': 'Uruguay'}
+    {'country': 'UY', 'language': 'es-uy', 'name': 'Uruguay'}
 ]
 
